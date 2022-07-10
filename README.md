0a0034646ddacf4dc930ee77382213038b0af79a
Dmitry Mokienko <d.mokienko@maroo.us>
2022-07-10 22:20:13 +0400

    feat: landing v1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # turbaza-lubava
+Landing
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "custom-gray": "#1F1867",
+                "custom-red": "#D24F6B",
+                "custom-green": "#588B71",
+            },
+            backgroundImage: {
+                river: "url('./src/assets/img/river_bg.png')",
+                river_vertical: "url('./src/assets/img/river_bg_vertical.jpg')",
+            },
+        },
+    },
+    plugins: [],
+};
