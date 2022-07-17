3e3165b2b1bcc0840e3e86e431005b34bd711d8d
Dmitry Mokienko <dmitrymokienko@gmail.com>
2022-07-17 22:40:53 +0300

    fix: improve

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
                 "custom-gray": "#1F1867",
                 "custom-red": "#D24F6B",
                 "custom-green": "#588B71",
+                "transparent": "transparent",
             },
             backgroundImage: {
                 river: "url('./src/assets/img/river_bg.png')",
